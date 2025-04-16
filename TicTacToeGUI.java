040fb25eac8bfaf4b182149c1e9ee69f0fcdc8ac
Luis Franco <luisfranco.business@gmail.com>
2025-04-15 22:53:04 -0600

    Added alternating colors

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -1,19 +1,31 @@
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
 
 public class TicTacToeGUI {
     public static void main(String[] args) {
+        // Executes runnable on AWT dispath thread
         SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 
     private static void createAndShowGUI() {
-        // Create the JFrame
+        // Initialize frame
         JFrame frame = new JFrame("Tic Tac Toe");
-        // Set the default close operation
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Set the size of the window (width, height)
-        frame.setSize(400, 400);
-        // Make the frame visible
+        frame.setSize(800, 800);
+
+        JPanel boardContainerPanel = new JPanel(new GridLayout(3, 3));
+
+        // Color alternating
+        for (int row = 0; row < 3; row++) {
+            for  (int col = 0; col < 3; col++) {
+                JPanel boardCell = new JPanel();
+                boolean isWhite = (row + col) % 2 == 0;
+                boardCell.setBackground(isWhite ? Color.WHITE : Color.GRAY);
+                boardContainerPanel.add(boardCell);
+            }
+        }
+
+        frame.add(boardContainerPanel, BorderLayout.CENTER);
         frame.setVisible(true);
-    }
+    }    
 }
